c7797ec425e5ee9423c9bfcf0f3b9bf49b0c2d54
kanhaiya kumar singh <skanahaiya05@gmail.com>
2026-07-02 20:26:51 +0530

    Debug again

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,53 @@
 import "../App.css";
-import profile from "../assets/image.png"; // ya profile.jpg
+import profile from "../assets/image.png";
+import { useEffect, useState } from "react"; // ya profile.jpg
 
 export default function Hero() {
+  const [line1, setLine1] = useState("");
+const [line2, setLine2] = useState("");
+
+useEffect(() => {
+  const text1 = "Hi, I'm";
+  const text2 = "Kanhaiya Kumar Singh";
+
+  let i = 0;
+
+  const firstTyping = setInterval(() => {
+    if (i < text1.length) {
+      setLine1(text1.slice(0, i + 1));
+      i++;
+    } else {
+      clearInterval(firstTyping);
+
+      let j = 0;
+
+      const secondTyping = setInterval(() => {
+        if (j < text2.length) {
+          setLine2(text2.slice(0, j + 1));
+          j++;
+        } else {
+          clearInterval(secondTyping);
+        }
+      }, 80);
+    }
+  }, 100);
+
+  return () => clearInterval(firstTyping);
+}, []);
+
   return (
    <section id="home" className="hero hero-flex">
 
   <div className="hero-left">
 
-    <h1 className="hero-title">
-      <span className="typing-line1">Hi, I'm</span>
-      <br />
-      <span className="typing-line2">Kanhaiya Kumar Singh</span>
-    </h1>
+<h1 className="hero-title">
+  {line1}
+  <br />
+  <span>{line2}</span>
+</h1>
 
     <p className="subtitle">
-      Mern Stack Developer | AI Web Applications | Software Engineer
+      MERN Stack Developer | AI Web Applications | Software Engineer
     </p>
         <div className="hero-buttons">
           <a href="#contact" className="btn">Contact Me</a>
